bdb48c41556673b111743765dffdb16fb85df81d
pasangeek <pasan.madusanka9@gmail.com>
2023-07-17 21:25:39 +0900

    common classes added

---
+++ app/src/main/java/com/example/recycleappv1/common/Result.kt
@@ -0,0 +1,8 @@
+package com.example.recycleappv1.common
+
+sealed class Result {
+    object Loading : Result()
+    data class Success<out R>(val result: R) : Result()
+    data class Failure(val error: String) : Result()
+
+}
