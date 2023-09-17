489320431fb4486359aa63b6d831a66d3130859f
pasangeek <pasan.madusanka9@gmail.com>
2023-09-17 23:03:25 +0900

    removing unwanted classes

--- app/src/main/java/com/example/recycleappv1/ui/onboarding/location/LocationViewModel.kt
+++ app/src/main/java/com/example/recycleappv1/ui/onboarding/location/LocationViewModel.kt
@@ -2,7 +2,6 @@ package com.example.recycleappv1.ui.onboarding.location
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.recycleappv1.data.model.LocationData
 import com.example.recycleappv1.data.sources.CommonImplRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,7 +13,7 @@ class LocationViewModel @Inject constructor(private val commonImplRepository: Co
     var lat : MutableLiveData<String> = MutableLiveData("")
     var long : MutableLiveData<String> = MutableLiveData("")
 
-    var location_data : MutableLiveData<LocationData> = MutableLiveData()
+    //var location_data : MutableLiveData<LocationData> = MutableLiveData()
     val city = MutableLiveData<String>()
     fun saveData(cityName:String) {
         commonImplRepository.saveCity(cityName)
