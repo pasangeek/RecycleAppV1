a844ccb96b1356b89dc0f6fc765eb2d731b8e58f
pasangeek <pasan.madusanka9@gmail.com>
2023-09-18 21:07:05 +0900

    alarm implimentation

--- app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderFragment.kt
+++ app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderFragment.kt
@@ -1,23 +1,29 @@
 package com.example.recycleappv1.ui.reminder
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-
+import com.example.recycleappv1.common.BaseFragment
 import com.example.recycleappv1.databinding.FragmentReminderBinding
+import com.example.recycleappv1.ui.reminder.notification.Notification
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 
 @AndroidEntryPoint
-class ReminderFragment : Fragment() {
+class ReminderFragment : BaseFragment() {
 
     private var _binding: FragmentReminderBinding? = null
-
+    private lateinit var pendingIntent: PendingIntent
     // This property is only valid between onCreateView and
     // onDestroyView.
+
+    private lateinit var myIntent: Intent
+    private var alarmManager: AlarmManager? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -36,6 +42,25 @@ class ReminderFragment : Fragment() {
 
         return root
     }
+    private fun setAlarm(target: Calendar) {
+        alarmManager?.set(AlarmManager.RTC_WAKEUP, target.timeInMillis, pendingIntent)
+        if (isAlarmSet()) {
+            println("Alarm set success")
+        }
+    }
+    private fun isAlarmSet(): Boolean {
+        val intent = Intent(
+            context,
+            Notification::class.java
+        ) // Replace YourReceiver with the appropriate receiver class
+        val pendingIntent =
+            PendingIntent.getBroadcast(
+                context, 0, intent,
+                PendingIntent.FLAG_NO_CREATE or PendingIntent.FLAG_IMMUTABLE
+            )
+        return pendingIntent != null
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
