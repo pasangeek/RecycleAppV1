4dcc7d9cc7eef441b56a86bcc9e14ae0ec0a0e66
pasangeek <pasan.madusanka9@gmail.com>
2023-09-18 21:23:28 +0900

    reminder viewmodel update

--- app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderFragment.kt
+++ app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderFragment.kt
@@ -7,7 +7,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import com.example.recycleappv1.common.BaseFragment
 import com.example.recycleappv1.databinding.FragmentReminderBinding
 import com.example.recycleappv1.ui.reminder.notification.Notification
@@ -17,15 +18,23 @@ import java.util.Calendar
 @AndroidEntryPoint
 class ReminderFragment : BaseFragment() {
 
-    private var _binding: FragmentReminderBinding? = null
+    private lateinit var binding: FragmentReminderBinding
     private lateinit var pendingIntent: PendingIntent
     // This property is only valid between onCreateView and
     // onDestroyView.
 
     private lateinit var myIntent: Intent
     private var alarmManager: AlarmManager? = null
-    private val binding get() = _binding!!
+    private val viewModel: ReminderViewModel by viewModels()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        alarmManager = ContextCompat.getSystemService(requireContext(), AlarmManager::class.java)
+        myIntent = Intent(requireContext(), Notification::class.java)
+        pendingIntent =
+            PendingIntent.getBroadcast(requireContext(), 0, myIntent, PendingIntent.FLAG_IMMUTABLE)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -33,14 +42,8 @@ class ReminderFragment : BaseFragment() {
     ): View {
 
 
-        val reminderViewModel =
-            ViewModelProvider(this).get(ReminderViewModel::class.java)
-
-        _binding = FragmentReminderBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        return root
+        binding = FragmentReminderBinding.inflate(inflater, container, false)
+        return binding.root
     }
     private fun setAlarm(target: Calendar) {
         alarmManager?.set(AlarmManager.RTC_WAKEUP, target.timeInMillis, pendingIntent)
@@ -62,8 +65,5 @@ class ReminderFragment : BaseFragment() {
     }
 
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+ 
 }
--- app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderViewModel.kt
+++ app/src/main/java/com/example/recycleappv1/ui/reminder/ReminderViewModel.kt
@@ -3,16 +3,47 @@ package com.example.recycleappv1.ui.reminder
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.recycleappv1.common.Result
+import com.example.recycleappv1.data.model.RecycleItemsData
+import com.example.recycleappv1.data.sources.CommonImplRepository
+import com.example.recycleappv1.data.sources.RecycleItemRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ReminderViewModel  @Inject constructor(
-
+    private val recycleItemRepo: RecycleItemRepo,
+    private val commonImplRepository: CommonImplRepository,
 ): ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is reminder Fragment"
     }
     val text: LiveData<String> = _text
+
+    val responseGetRecyclerItems = MutableLiveData<Result>()
+
+    fun getRecyclerDataByWasteType() {
+        responseGetRecyclerItems.postValue(Result.Loading)
+        recycleItemRepo.getRecyclerDataByWasteType(getSavedCity()?:"", "Non Burnable")
+            .addOnSuccessListener { queryDocumentSnapshots ->
+                val list = ArrayList<RecycleItemsData>()
+                queryDocumentSnapshots.documents.forEach {
+                    val item = it.toObject(RecycleItemsData::class.java)
+                    if (item != null) {
+                        list.add(item)
+                    }
+
+                }
+                responseGetRecyclerItems.postValue(Result.Success<List<RecycleItemsData>>(list))
+            }.addOnFailureListener {
+                responseGetRecyclerItems.postValue(Result.Failure("Failed data loading"))
+            }
+    }
+
+    private fun getSavedCity() = commonImplRepository.getSavedCity()
+
+    fun getNonBurnableReminderStatus() : Boolean = commonImplRepository.getNonBurnableReminderStatus()
+    fun saveNonBurnableReminderStatus(status: Boolean) = commonImplRepository.saveNonBurnableReminderStatus(status)
+
 }
